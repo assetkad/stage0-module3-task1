3c911db927f22b7c751d37bbe495b34e0fa9a8b1
Asset Kad <asset_kad@mail.ru>
2023-05-04 15:41:47 +0500

    module 3, all tasks are completed0

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -1,7 +1,8 @@
 package lang.print.gaps.task1;
 
 public class PrintWordsApp {
-    public static void main(String[] args) {
-        //write your code here
-    }
+  public static void main(String[] args) {
+    String newLine = System.getProperty("line.separator");
+    System.out.println("java" + newLine + "is" + newLine + "smth");
+  }
 }
